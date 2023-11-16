affc56dbba7fd483bad123b5fb93154882d3eaaf
NIHAOAWA <142315379+NIHAOAWA@users.noreply.github.com>
2023-11-16 19:14:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
-int yes,no;
-bool work(int work){
+bool work(){
+    bool completed;
+    cout << "是否完成作业？（1代表是，0代表否）";
+    cin >> completed;
+    if (completed){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
+int main(){
+    cout << work() << endl;
+    return 0;
 }
